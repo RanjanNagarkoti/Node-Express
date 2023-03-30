bd79d092c6de446a332ec508fadf27faabf7c150
Ranjan Nagarkoti <ranjan.ngr2058@gmail.com>
2023-03-30 18:09:55 +0545

    bug fixed on user schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,37 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register the user" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const userAvailable = await User.findOne({ email });
+
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("Emaily already exists our db!");
+  }
+
+  //bcrypt password
+  // const hashedPassword = await bcrypt.hash(password, 10);
+
+  // const user = await User.create({
+  //   name,
+  //   email,
+  //   hashedPassword,
+  // });
+
+  // if (user) {
+  //   res.status(201).json({ _id: user.id, email: user.email });
+  // } else {
+  //   res.status(400);
+  //   throw new Error("User data is not valid");
+  // }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
